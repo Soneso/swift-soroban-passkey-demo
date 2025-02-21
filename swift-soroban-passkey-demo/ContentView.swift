1c92c685c06b8af7fa673d186127636b6de152fc
Christian Rogobete <c.rogobete@soneso.com>
2025-02-21 13:34:23 +0100

    Initial Commit

---
+++ swift-soroban-passkey-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-soroban-passkey-demo
+//
+//  Created by Christian Rogobete on 21.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-soroban-passkey-demo/swift_soroban_passkey_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_soroban_passkey_demoApp.swift
+//  swift-soroban-passkey-demo
+//
+//  Created by Christian Rogobete on 21.02.25.
+//
+
+import SwiftUI
+
+@main
+struct swift_soroban_passkey_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
